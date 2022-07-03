407678861395f3fd941c9fe58b84e6482d9915ec
WizardMaster38 <lolifarted96@gmail.com>
2022-07-03 11:52:16 -0400

    a

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from tkinter import *
+from sqlite3 import *
+from math import *
+
+class InputManager:
+    str = "a"
+
+
+class DatabaseManager:
+    str = "a"
+
+def Main():
+    return    
+    
+
+if __name__ == "__main__":
+    Main();
